953eac2dcab3bd20d9359ac4fcc5d821749163d5
Angelina <7austinova@gmail.com>
2022-12-17 17:11:01 +0500

    almost made field drawing mechanism

--- field.py
+++ field.py
@@ -1,8 +1,9 @@
 import pygame as pg
 import numpy as np
+from PIL import ImageColor
 
 
-class Field:
+class Board:
     def __init__(self, width, height, color1, color2, color3, color4):
         self.width = width
         self.height = height
@@ -45,31 +46,88 @@ class Field:
 
 
 class Element(pg.sprite.Sprite):
-    def __init__(self, left, top, cell_size, color, *groups):
+    def __init__(self, row, col, top, cell_size, color, *groups):
         super(Element, self).__init__(*groups)
-        # self.rect = left, top, cell_size, color
+        self.image = pg.Surface((cell_size, cell_size))
+        self.rect = self.image.get_rect()
+        self.color = color
+        pg.draw.rect(self.image, self.color, self.rect)
+        self.rect.x = col * cell_size
+        self.rect.y = top + row * cell_size
 
-    def update(self, frames, speed) -> None:
+    def update(self) -> None:
         pass
 
 
-all_sprites = pg.sprite.Group()
-for r in range(8):
-    for c in range(8):
-        Element(75, 'red', all_sprites)
+class Field:
+    def __init__(self, width, height, color1, color2, color3, color4):
+        self.width = width
+        self.height = height
+        self.left = 0
+        self.top = 75
+        self.cell_size = 75
+        self.left_top = color1
+        self.right_top = color2
+        self.left_bottom = color3
+        self.right_bottom = color4
+        self.all_elems = pg.sprite.Group()
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self):
+        r, c = 0, 0
+        drawn_1 = False
+        coeff_h_left = (self.left_bottom - self.left_top) // self.height
+        coeff_h_right = (self.right_bottom - self.right_top) // self.height
+        for n in range(self.height * self.width + self.width + 1):
+            coeff_w = ((self.right_top + (coeff_h_right * r)) - (self.left_top + (coeff_h_left * r))) // self.width
+            if c < self.width: # or (c == 0 and r == 0)
+                Element(r, c, self.top, self.cell_size,
+                        self.left_top + (coeff_h_left * r) + (coeff_w * c), self.all_elems)
+                print(c, r)
+                c += 1
+            else:
+                c = 0
+                r += 1
+                Element(r, c, self.top, self.cell_size, self.left_top + (coeff_h_left * r), self.all_elems)
+                drawn_1 = True
+                print('element was drown on the left')
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def get_cell(self, mouse_coord):
+        x, y = mouse_coord
+        r, c = (y - self.top) // self.cell_size, (x - self.left) // self.cell_size
+        if 0 <= r < self.height and 0 <= c < self.width:
+            return r, c
+        return None
+
+    def on_click(self, cell):
+        print(cell)
+
 
 if __name__ == '__main__':
     pg.init()
-    screen = pg.display.set_mode((500, 650))
-    game = Field(6, 8, (22, 235, 86), (225, 255, 55), (54, 39, 152), (220, 87, 82))
-    game.set_view(25, 25, 75)
+    screen = pg.display.set_mode((900, 900))
+    lt = np.array((239, 38, 226))
+    rt = np.array((24, 240, 168))
+    lb = np.array((247, 246, 198))
+    rb = np.array((16, 6, 107))
+    level = Field(10, 13, lt, rt, lb, rb)
+    level.set_view(0, 75, 50)
     running = True
+    level.render()
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
         screen.fill((0, 0, 0))
-        game.render(screen)
-        all_sprites.draw(screen)
+
+        level.all_elems.draw(screen)
         pg.display.flip()
     pg.quit()
