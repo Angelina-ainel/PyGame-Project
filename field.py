8433a366ddca4803dd28ec51b75adbc3a9e4462f
Angelina <7austinova@gmail.com>
2022-12-20 22:54:44 +0500

    this thing is working without visible bugs now

--- field.py
+++ field.py
@@ -3,48 +3,6 @@ import numpy as np
 from PIL import ImageColor
 
 
-class Board:
-    def __init__(self, width, height, color1, color2, color3, color4):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 50
-        self.top = 50
-        self.cell_size = 50
-        self.left_top = color1
-        self.right_top = color2
-        self.left_bottom = color3
-        self.right_bottom = color4
-        self.elements = []
-
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def render(self, surface):
-        for r in range(len(self.board)):
-            for c in range(len(self.board[r])):
-                pg.draw.rect(surface, 'white',
-                             (self.left + c * self.cell_size,
-                              self.top + r * self.cell_size,
-                              self.cell_size, self.cell_size))
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
-
-    def get_cell(self, mouse_coord):
-        x, y = mouse_coord
-        r, c = (y - self.top) // self.cell_size, (x - self.left) // self.cell_size
-        if 0 <= r < self.height and 0 <= c < self.width:
-            return r, c
-        return None
-
-    def on_click(self, cell):
-        print(cell)
-
-
 class Element(pg.sprite.Sprite):
     def __init__(self, row, col, top, cell_size, color, *groups):
         super(Element, self).__init__(*groups)
@@ -79,22 +37,20 @@ class Field:
 
     def render(self):
         r, c = 0, 0
-        drawn_1 = False
         coeff_h_left = (self.left_bottom - self.left_top) // self.height
         coeff_h_right = (self.right_bottom - self.right_top) // self.height
-        for n in range(self.height * self.width + self.width + 1):
+        for n in range(self.height * self.width + self.width):
             coeff_w = ((self.right_top + (coeff_h_right * r)) - (self.left_top + (coeff_h_left * r))) // self.width
-            if c < self.width: # or (c == 0 and r == 0)
+            if c < self.width:
+                if c == 0 and r != 0:
+                    c += 1
                 Element(r, c, self.top, self.cell_size,
                         self.left_top + (coeff_h_left * r) + (coeff_w * c), self.all_elems)
-                print(c, r)
                 c += 1
             else:
                 c = 0
                 r += 1
                 Element(r, c, self.top, self.cell_size, self.left_top + (coeff_h_left * r), self.all_elems)
-                drawn_1 = True
-                print('element was drown on the left')
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -113,12 +69,12 @@ class Field:
 
 if __name__ == '__main__':
     pg.init()
-    screen = pg.display.set_mode((900, 900))
-    lt = np.array((239, 38, 226))
-    rt = np.array((24, 240, 168))
-    lb = np.array((247, 246, 198))
-    rb = np.array((16, 6, 107))
-    level = Field(10, 13, lt, rt, lb, rb)
+    screen = pg.display.set_mode((950, 950))
+    lt = np.array((222, 200, 100))
+    rt = np.array((16, 117, 249))
+    lb = np.array((34, 244, 111))
+    rb = np.array((230, 11, 255))
+    level = Field(13, 16, lt, rt, lb, rb)
     level.set_view(0, 75, 50)
     running = True
     level.render()
