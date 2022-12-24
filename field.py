4823e53c7f72b1a7cca79885f787bb7a9d1b7072
Angelina <7austinova@gmail.com>
2022-12-24 17:12:49 +0500

    work on mixing in progress

--- field.py
+++ field.py
@@ -3,9 +3,11 @@ import numpy as np
 from PIL import ImageColor
 import random
 
+fixed_elems = ['4 corners', 'whole frame', 'vertical lines', 'horizontal lines']
+
 
 class Element(pg.sprite.Sprite):
-    def __init__(self, row, col, top, cell_size, color, *groups):
+    def __init__(self, row, col, top, cell_size, color, fixed, id, *groups):
         super(Element, self).__init__(*groups)
         self.image = pg.Surface((cell_size, cell_size))
         self.rect = self.image.get_rect()
@@ -13,13 +15,18 @@ class Element(pg.sprite.Sprite):
         pg.draw.rect(self.image, self.color, self.rect)
         self.rect.x = col * cell_size
         self.rect.y = top + row * cell_size
+        self.id = id
+        self.fixed = fixed
+        if self.fixed:
+            pg.draw.circle(self.image, 'black', self.rect.center, 10)
+        self.mixed = False
 
-    def update(self) -> None:
+    def update(self):
         pass
 
 
 class Field:
-    def __init__(self, width, height, color1, color2, color3, color4):
+    def __init__(self, width, height, color1, color2, color3, color4, type):
         self.width = width
         self.height = height
         self.left = 0
@@ -30,44 +37,81 @@ class Field:
         self.left_bottom = color3
         self.right_bottom = color4
         self.all_elems = pg.sprite.Group()
-        self.colors = []
+        self.mixed_elems = pg.sprite.Group()
+        self.list_elems = []
+        self.fixed_elems = type
+        self.mixed = False
 
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
         self.cell_size = cell_size
 
+    def check_fixing(self, r, c):
+        if self.fixed_elems == '4 corners':
+            if (c == 0 and r == 0) or (c == 0 and r == self.height - 1) \
+                    or (c == self.width - 1 and r == 0) or (c == self.width - 1 and r == self.height - 1):
+                return True
+            return False
+
     def render(self):
+        id = 1
         r, c = 0, 0
         coeff_h_left = np.trunc((self.left_bottom - self.left_top) / (self.height - 1))
         coeff_h_right = np.trunc((self.right_bottom - self.right_top) / (self.height - 1))
         for n in range(self.height * self.width):
             coeff_w = np.trunc(((self.right_top + (coeff_h_right * r)) -
                        (self.left_top + (coeff_h_left * r))) / (self.width - 1))
+            # print(r, c)
             if c < self.width:
                 if c == 0 and r != 0:
                     c += 1
-
+                fixing = self.check_fixing(r, c)
                 color = self.left_top + (coeff_h_left * r) + (coeff_w * c)
                 # print(color)
-                Element(r, c, self.top, self.cell_size, color, self.all_elems)
+                sprite = Element(r, c, self.top, self.cell_size, color, fixing, id, self.all_elems)
                 c += 1
-                self.colors.append(color)
+                id += 1
+                self.list_elems.append(sprite)
             else:
                 c = 0
                 r += 1
-                Element(r, c, self.top, self.cell_size, self.left_top + (coeff_h_left * r), self.all_elems)
-                self.colors.append(self.left_top + (coeff_h_left * r))
-        # print(len(self.colors))
+                fixing = self.check_fixing(r, c)
+                sprite = Element(r, c, self.top, self.cell_size,
+                                 self.left_top + (coeff_h_left * r), fixing, id, self.all_elems)
+                id += 1
+                self.list_elems.append(sprite)
+        # self.mix_elements()
 
     def mix_elements(self):
-        random.shuffle(self.all_elems.sprites())
-    # записала идею в google doc
-        pass
+        # можно сделать так и вставять нефиксированные плитки на опр. позицию insert'ом
+        # np_elems = np.array(self.list_elems)
+        # not_fixed_tiles = list(filter(lambda el: not el.fixed, self.list_elems))
+        for i in range(len(self.list_elems)):
+            if not self.list_elems[i].mixed and not self.list_elems[i].fixed:
+                filtered = [tile for tile in self.list_elems if
+                            not tile.mixed and not tile.fixed and tile != self.list_elems[i]]
+                for el in filtered:
+                    print(el.id, end='; ')
+                # для фильтра написать отдельную функцию
+                # for el in list(filter(lambda tile: not tile.fixed and tile != self.list_elems[i], self.list_elems)):
+                    # print(el.id, end='; ')
+                pair = random.choice([tile for tile in self.list_elems if
+                                      not tile.mixed and not tile.fixed and tile != self.list_elems[i]])
+                print(self.list_elems[i].id, pair.id)
+                self.list_elems[i] = pair
+                self.list_elems[self.list_elems.index(pair)] = self.list_elems[i]
+                pair.mixed = True
+                self.list_elems[i].mixed = True
+        self.mixed_elems.add(*self.list_elems)
+        for el in self.list_elems:
+            print(el.id, end=', ')
+        # проверить это и отрисовать
 
     def make_array(self):
-        np_colors = np.array(self.colors)
-
+        np_colors = np.array(self.list_elems)
+        for el in self.list_elems:
+            print(el.fixed)
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -87,20 +131,23 @@ class Field:
 if __name__ == '__main__':
     pg.init()
     screen = pg.display.set_mode((950, 950))
-    lt = np.array((98, 0, 5))
-    rt = np.array((65, 120, 8))
+    lt = np.array((234, 31, 255))
+    rt = np.array((255, 234, 31))
     lb = np.array((220, 210, 255))
     rb = np.array((30, 2, 140))
 
-    level = Field(6, 8, lt, rt, lb, rb)
+    level = Field(6, 8, lt, rt, lb, rb, '4 corners')
     level.set_view(0, 75, 50)
     running = True
     level.render()
+    level.mix_elements()
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
         screen.fill((0, 0, 0))
+        # level.mixed_elems.draw(screen)
         level.all_elems.draw(screen)
+
         pg.display.flip()
     pg.quit()
