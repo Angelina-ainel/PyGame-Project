db24c98541a1e570f14dfb9cae0c5a9a58697429
Angelina <7austinova@gmail.com>
2022-12-24 23:44:01 +0500

    finished shuffling

--- field.py
+++ field.py
@@ -6,6 +6,12 @@ import random
 fixed_elems = ['4 corners', 'whole frame', 'vertical lines', 'horizontal lines']
 
 
+def condition(elem):
+    if not elem.mixed and not elem.fixed:
+        return True
+    return False
+
+
 class Element(pg.sprite.Sprite):
     def __init__(self, row, col, top, cell_size, color, fixed, id, *groups):
         super(Element, self).__init__(*groups)
@@ -13,13 +19,13 @@ class Element(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.color = color
         pg.draw.rect(self.image, self.color, self.rect)
+        self.fixed = fixed
+        if self.fixed:
+            pg.draw.circle(self.image, 'black', self.rect.center, 3)
+        self.mixed = False
         self.rect.x = col * cell_size
         self.rect.y = top + row * cell_size
         self.id = id
-        self.fixed = fixed
-        if self.fixed:
-            pg.draw.circle(self.image, 'black', self.rect.center, 10)
-        self.mixed = False
 
     def update(self):
         pass
@@ -37,7 +43,6 @@ class Field:
         self.left_bottom = color3
         self.right_bottom = color4
         self.all_elems = pg.sprite.Group()
-        self.mixed_elems = pg.sprite.Group()
         self.list_elems = []
         self.fixed_elems = type
         self.mixed = False
@@ -62,13 +67,11 @@ class Field:
         for n in range(self.height * self.width):
             coeff_w = np.trunc(((self.right_top + (coeff_h_right * r)) -
                        (self.left_top + (coeff_h_left * r))) / (self.width - 1))
-            # print(r, c)
             if c < self.width:
                 if c == 0 and r != 0:
                     c += 1
                 fixing = self.check_fixing(r, c)
                 color = self.left_top + (coeff_h_left * r) + (coeff_w * c)
-                # print(color)
                 sprite = Element(r, c, self.top, self.cell_size, color, fixing, id, self.all_elems)
                 c += 1
                 id += 1
@@ -81,37 +84,14 @@ class Field:
                                  self.left_top + (coeff_h_left * r), fixing, id, self.all_elems)
                 id += 1
                 self.list_elems.append(sprite)
-        # self.mix_elements()
 
     def mix_elements(self):
-        # можно сделать так и вставять нефиксированные плитки на опр. позицию insert'ом
-        # np_elems = np.array(self.list_elems)
-        # not_fixed_tiles = list(filter(lambda el: not el.fixed, self.list_elems))
         for i in range(len(self.list_elems)):
             if not self.list_elems[i].mixed and not self.list_elems[i].fixed:
-                filtered = [tile for tile in self.list_elems if
-                            not tile.mixed and not tile.fixed and tile != self.list_elems[i]]
-                for el in filtered:
-                    print(el.id, end='; ')
-                # для фильтра написать отдельную функцию
-                # for el in list(filter(lambda tile: not tile.fixed and tile != self.list_elems[i], self.list_elems)):
-                    # print(el.id, end='; ')
-                pair = random.choice([tile for tile in self.list_elems if
-                                      not tile.mixed and not tile.fixed and tile != self.list_elems[i]])
-                print(self.list_elems[i].id, pair.id)
-                self.list_elems[i] = pair
-                self.list_elems[self.list_elems.index(pair)] = self.list_elems[i]
+                pair = random.choice(list(filter(condition, self.list_elems)))
+                self.list_elems[i].rect.topleft, pair.rect.topleft = pair.rect.topleft, self.list_elems[i].rect.topleft
                 pair.mixed = True
                 self.list_elems[i].mixed = True
-        self.mixed_elems.add(*self.list_elems)
-        for el in self.list_elems:
-            print(el.id, end=', ')
-        # проверить это и отрисовать
-
-    def make_array(self):
-        np_colors = np.array(self.list_elems)
-        for el in self.list_elems:
-            print(el.fixed)
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -131,10 +111,10 @@ class Field:
 if __name__ == '__main__':
     pg.init()
     screen = pg.display.set_mode((950, 950))
-    lt = np.array((234, 31, 255))
-    rt = np.array((255, 234, 31))
-    lb = np.array((220, 210, 255))
-    rb = np.array((30, 2, 140))
+    lt = np.array((31, 255, 255))
+    rt = np.array((90, 255, 106))
+    lb = np.array((255, 118, 233))
+    rb = np.array((255, 250, 63))
 
     level = Field(6, 8, lt, rt, lb, rb, '4 corners')
     level.set_view(0, 75, 50)
@@ -146,7 +126,6 @@ if __name__ == '__main__':
             if event.type == pg.QUIT:
                 running = False
         screen.fill((0, 0, 0))
-        # level.mixed_elems.draw(screen)
         level.all_elems.draw(screen)
 
         pg.display.flip()
